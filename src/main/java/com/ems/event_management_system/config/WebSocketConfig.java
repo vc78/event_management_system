17ecc1c0b1ed3c95e3ed9f19c98436d5bef1cd74
venkat <venkatbodduluri78@gmail.com>
2026-07-11 21:48:08 +0530

    fix: Phase A5 - WebSocket CORS tightened

--- src/main/java/com/ems/event_management_system/config/WebSocketConfig.java
+++ src/main/java/com/ems/event_management_system/config/WebSocketConfig.java
@@ -35,7 +35,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
+                .setAllowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*")
                 .withSockJS();
     }
 
